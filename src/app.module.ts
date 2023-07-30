d8857dcc86850a8c6336405d00634bf830ff49a7
shinobu_takahata <shinoshino2335@gmail.com>
2023-07-30 19:08:19 +0900

    jwtの導入 authModuleの設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { join } from 'path';
 import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { UserModule } from './user/user.module';
     TaskModule,
     PrismaModule,
     UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  // デフォルトの認証情報をjwtに設定
+  imports: [
+    UserModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthResolver, AuthService],
+})
+export class AuthModule {}
